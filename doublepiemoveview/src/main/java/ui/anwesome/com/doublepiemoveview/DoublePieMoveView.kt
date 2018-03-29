5db24b183fae59afcf5d769988254e3c03974102
Anwesh43 <anweshthecool0@gmail.com>
2018-03-29 07:43:42 +0530

    adding doublepiemoveview

--- doublepiemoveview/src/main/java/ui/anwesome/com/doublepiemoveview/DoublePieMoveView.kt
+++ doublepiemoveview/src/main/java/ui/anwesome/com/doublepiemoveview/DoublePieMoveView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.doublepiemoveview
 /**
  * Created by anweshmishra on 29/03/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class DoublePieMoveView (ctx : Context) : View(ctx) {
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
+    }
+}
